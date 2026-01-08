ccdbf34234525855c84cc20d15270908f482ee66
pradeep s <pirathims@gmail.com>
2026-01-08 09:11:10 +0530

    fix login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
+
 import "./globals.css"
 
 const _geist = Geist({ subsets: ["latin"] })
@@ -25,7 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <body className="font-sans antialiased">
         {children}
-        <Analytics />
       </body>
     </html>
   )
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,13 +17,6 @@ export default function LoginPage() {
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    setEmail("admin@firebase.com")
-  }, [])
-  useEffect(() => {
-    setPassword("admin123")
-  }, [])
-
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault()
     setError("")
